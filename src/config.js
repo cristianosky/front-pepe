a31a2097e18d6495cd394b86f69171a3cb8d23b7
Cristian Villalba <cristianvillalba1999@gmail.com>
2026-04-25 12:43:59 -0500

    Server de pruebas

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://192.168.1.10:4000/api';
-export const SOCKET_URL = 'http://192.168.1.10:4000';
+export const API_URL = 'http://64.23.173.134:4000/api';
+export const SOCKET_URL = 'http://64.23.173.134:4000';
